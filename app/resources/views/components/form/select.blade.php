0b7b4037888dfd01eac5b3f4c67b7f6e7bd68992
joe <iamjothees@gmail.com>
2026-09-19 00:54:26 +0530

    fix: ensure entire TomSelect wrapper area is clickable to open dropdown

--- app/resources/views/components/form/select.blade.php
+++ app/resources/views/components/form/select.blade.php
@@ -29,6 +29,15 @@
                 }
             }
         });
+        
+        ts.wrapper.addEventListener('click', (e) => {
+            if (!ts.isOpen) {
+                ts.open();
+                if (ts.control_input) {
+                    ts.control_input.focus();
+                }
+            }
+        });
     }
 }">
     <select x-ref="select" {{ $attributes->merge(['class' => 'w-full bg-gray-50 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-md px-3 py-2 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600']) }} {{ $multiple ? 'multiple' : '' }}>
