85f6f3a21a72831fa29fbbbff4434c063817adfb
joe <iamjothees@gmail.com>
2026-09-24 06:43:23 +0530

    refactor: rename ButtonManager callbacks to purely hardware-agnostic terms

--- hardware/macropad/src/hardware/ButtonManager.h
+++ hardware/macropad/src/hardware/ButtonManager.h
@@ -7,31 +7,31 @@ class ButtonManager {
 private:
     OneButton btn1;
     OneButton btn2;
-    std::function<void()> onActionClick;
-    std::function<void()> onActionLongPress;
-    std::function<void()> onCycleClick;
+    std::function<void()> onBtn1Click;
+    std::function<void()> onBtn1LongPress;
+    std::function<void()> onBtn2Click;
 
 public:
     ButtonManager(int pin1, int pin2) : btn1(pin1, true), btn2(pin2, true) {}
 
-    void begin(std::function<void()> actionClick, std::function<void()> actionLongPress, std::function<void()> cycleClick) {
-        onActionClick = actionClick;
-        onActionLongPress = actionLongPress;
-        onCycleClick = cycleClick;
+    void begin(std::function<void()> btn1Click, std::function<void()> btn1LongPress, std::function<void()> btn2Click) {
+        onBtn1Click = btn1Click;
+        onBtn1LongPress = btn1LongPress;
+        onBtn2Click = btn2Click;
 
         btn1.attachClick([](void *ptr) {
             ButtonManager *mgr = (ButtonManager *)ptr;
-            if (mgr->onActionClick) mgr->onActionClick();
+            if (mgr->onBtn1Click) mgr->onBtn1Click();
         }, this);
         
         btn1.attachLongPressStart([](void *ptr) {
             ButtonManager *mgr = (ButtonManager *)ptr;
-            if (mgr->onActionLongPress) mgr->onActionLongPress();
+            if (mgr->onBtn1LongPress) mgr->onBtn1LongPress();
         }, this);
         
         btn2.attachClick([](void *ptr) {
             ButtonManager *mgr = (ButtonManager *)ptr;
-            if (mgr->onCycleClick) mgr->onCycleClick();
+            if (mgr->onBtn2Click) mgr->onBtn2Click();
         }, this);
     }
 
