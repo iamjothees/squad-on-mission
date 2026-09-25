d03f23ba17422d65462fbda0b239a1286e7afc07
joe <iamjothees@gmail.com>
2026-09-26 03:00:06 +0530

    style: migrate TomSelect component CSS to use dynamic semantic brand variables

--- app/resources/views/components/form/select.blade.php
+++ app/resources/views/components/form/select.blade.php
@@ -40,7 +40,7 @@
  });
  }
 }">
- <select x-ref="select" {{ $attributes->merge(['class' => 'w-full bg-bg border border-border rounded-md px-3 py-2 text-fg focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600']) }} {{ $multiple ? 'multiple' : '' }}>
+ <select x-ref="select" {{ $attributes->merge(['class' => 'w-full bg-bg border border-border rounded-md px-3 py-2 text-fg focus:outline-none focus:ring-2 focus:ring-accent']) }} {{ $multiple ? 'multiple' : '' }}>
  {{ $slot }}
  </select>
 </div>
